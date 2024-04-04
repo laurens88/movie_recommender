1f9f55a67b92c50bd5807213c0edc6e0cbf79823
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-04-04 14:26:48 +0200

    Fix routing

--- movierecommender/src/App.js
+++ movierecommender/src/App.js
@@ -1,16 +1,20 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import LoginPage from './screens/loginPage';
 import LoginForm from './components/loginForm';
 
+
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<h1>Test</h1>} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </header>
+    </div>
   );
 }
 
--- movierecommender/src/index.js
+++ movierecommender/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <h1>Home</h1> },
+  { path: '/login', element: <h1>Login</h1> },
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
@@ -15,3 +23,25 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+// import * as React from "react";
+// import * as ReactDOM from "react-dom/client";
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+// } from "react-router-dom";
+// import "./index.css";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello world!</div>,
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
