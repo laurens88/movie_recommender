aa8194b05aa3116ff8196bbb6c52e83317038391
Laurens de Bruin <laurens.de.bruin99@gmail.com>
2024-04-15 21:43:10 +0200

    Add grid and responsive design to categories page

--- movierecommender/src/screens/categoriesPage.js
+++ movierecommender/src/screens/categoriesPage.js
@@ -17,65 +17,65 @@ function CategoriesPage() {
         <div>
             <h1 className={styles.h1}>Which if these categories do you like?</h1>
             <h3 className={styles.h3}>Select at least one</h3>
-            <Grid container spacing={4}>
-            <Grid item xs={4}>
+            <Grid container spacing={4} justifyContent="center">
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <Tooltip title="Movies such as: " placement='bottom'>
             <PrettyButton
                 text="Uplifting"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#A7C7E7"
             />
             </Tooltip>
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <PrettyButton 
                 text="Inspiring"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#A7C7E7"
             />
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <PrettyButton 
                 text="Hopeful"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#A7C7E7"
             />
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <PrettyButton 
                 text="Sad"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#A7C7E7"
             />
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <PrettyButton 
                 text="Informative"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#A7C7E7"
             />
             </Grid>
 
-            <Grid item xs={4}>
+            <Grid item lg={4} sm={6} xs={6} display="flex" justifyContent="center">
             <PrettyButton 
                 text="No preference"
-                width='200px' 
-                height='150px' 
-                fontSize='25px' 
+                width='min(30vw, 200px)'
+                height='min(25vw, 150px)'
+                fontSize='min(4vw, 25px)' 
                 color="#3742A2"
             />
             </Grid>
